137427bfd2c504c630fefae899eef3c67265c675
Marc O'Morain <github.com@marcomorain.com>
2013-12-02 20:00:01 +0000

    Progress  bar

--- duplickatr.rb
+++ duplickatr.rb
@@ -8,13 +8,50 @@ require 'leveldb'
 require 'pathname'
 require 'open-uri'
 require 'find'
+require 'filesize'
+require 'ruby-progressbar'
+
 
 per_page = 100
-count    = 0
+jobs     = 0
 queued   = 0
 
+class SafeProgressBar
+  def initialize()
+    @lock = Mutex.new
+    @bar  = ProgressBar.create(:format => '%a %B %p%% %t')
+
+    Thread.new do
+      while true do
+        @log.synchronize { @bar.refresh }
+        sleep 1
+      end
+    end
+  end
+
+  def increment
+    @lock.synchronize { @bar.increment }
+  end
+
+  def title=(title)
+    @lock.synchronize { @bar.title = title }
+  end
+
+  def total=(total)
+    @lock.synchronize { @bar.total = total }
+  end
+
+  def log(s)
+    @lock.synchronize { @bar.log(s) }
+  end
+
+  def progress_mark=(p)
+    @lock.synchronize { @bar.progress_mark = p }
+  end
+end
+
 semaphore       = Mutex.new
-queue           = WorkQueue.new(64)
+queue           = WorkQueue.new(32)
 db              = LevelDB::DB.new File.join(File.expand_path('~'), '.duplickatr.ldb')
 sha_tag         = /hash:sha1=(\h{40})/
 min_upload_date = db['meta:min_upload_date'] ||= Time.new(2004, 2, 1).to_i
@@ -59,7 +96,9 @@ else
   end
 end
 
-puts("Starting downloads from #{Time.at(min_upload_date.to_i)}")
+$progress = SafeProgressBar.new
+
+$progress.log("Starting downloads from #{Time.at(min_upload_date.to_i)}")
 
 def make_hash_tag(hash)
   raise 'Unknown SHA1' if hash.empty?
@@ -80,7 +119,6 @@ Photo = Struct.new(:id, :url, :hash) do
   end
 end
 
-
 def sha1_digest_of(content)
   Digest::SHA1.hexdigest(content)
 end
@@ -94,27 +132,32 @@ DownloadJob = Struct.new(:semaphore, :queue, :db, :photo) do
                               :tags     => photo.hash_tag)
         photo.store_metadata_in(db)
       end
-      puts("#{queue.cur_tasks} jobs remain on queue. Done #{photo.url} as #{photo.hash}")
+      $progress.log("#{queue.cur_tasks} jobs remain on queue. Done #{photo.url} as #{photo.hash}")
+      $progress.increment
     rescue Exception => e
-      puts(e)
-      puts(e.backtrace.join("\n"))
+      $progress.log(e)
     end
   end
 end
 
 UploadJob = Struct.new(:semaphore, :queue, :db, :photo, :hash) do
   def upload
+    size = Filesize.new(File.stat(photo).size).pretty
+    $progress.log("Uploading #{photo} #{size}")
+    $progress.increment
     flickr.upload_photo(photo,  :tags      => make_hash_tag(hash),
                                 :is_public => 0,
                                 :is_friend => 0,
                                 :is_family => 1)
-    puts("#{hash} uploaded #{queue.cur_tasks} jobs remain on the queue")
+    #$progress.log("#{photo} uploaded #{queue.cur_tasks} jobs remain on the queue")
+    
   end
 end
-
+count = 0
+$progress.title = "Downloading photos"
 for page in 1..500 do
 
-  puts("Downloading photo metadata from #{per_page * (page-1)} to #{page * per_page}")
+  $progress.log("Downloading photo metadata from #{per_page * (page-1)} to #{page * per_page}")
 
   photos = semaphore.synchronize do
     flickr.people.getPhotos(:user_id         => login.id,
@@ -125,12 +168,11 @@ for page in 1..500 do
   end
   break if photos.size == 0
 
-  puts("Downloaded data for #{photos.size} photos")
+  $progress.log("Downloaded data for #{photos.size} photos")
 
   photos.each do |p|
     hashes = p.machine_tags.split.map { |tag| sha_tag.match(tag) }
     if hashes.empty?
-      puts(p.id)
       queued = queued + 1
       photo  = Photo.new(p.id, p.url_o, '')
       job    = DownloadJob.new(semaphore, queue, db, photo)
@@ -140,51 +182,50 @@ for page in 1..500 do
         photo = Photo.new(p.id, p.url_o, hashes.first[1])
         semaphore.synchronize { photo.store_metadata_in(db) }
      rescue Exception => e
-        puts('Something odd is up with this photo:')
-        puts(e)
-        puts(p)
+        $progress.log("Something odd is up with this photo: #{e} #{p}")
      end
     end
   end
   count += photos.size
 end
-puts("Total: #{count} to process: #{queued}")
+$progress.total = queued
+$progress.log("Total: #{count} to process: #{queued}")
 
 queue.join
+queue           = WorkQueue.new(32)
 db['meta:min_upload_date'] = started_at
-puts("Complete. Download will start at #{Time.at(started_at)} next time")
-
+$progress = SafeProgressBar.new
+$progress.log("Download Complete. Download will start at #{Time.at(started_at)} next time")
 
 iphoto_masters = File.join(`defaults read com.apple.iPhoto RootDirectory`.strip, '/Masters/')
-puts(iphoto_masters)
 count = 0
 
-puts("Scanning iPhoto directory")
+$progress.log("Scanning iPhoto directory")
 files = Find.find(iphoto_masters).reject {|f| FileTest.directory?(f) }.sort
-puts("Scan complete")
+$progress.total = files.size
 
 
-max_files_per_job = 512
+files.each do |path|
 
-files.each_slice(max_files_per_job).each do |file_set|
-  queue.enqueue_b do
-    file_set.each do |path|
-      hash = sha1_digest_of(File.read(path))
-      puts("Local image file: #{path} hash: #{hash}")
-      existing = db["photo:sha1:#{hash}"]
-
-      if existing.nil?
-        job = UploadJob.new(semaphore, queue, db, path, hash)
-        queue.enqueue_b { job.upload }
-      else
-        #puts("Found exiting tag for #{hash} - not uploading")
-      end
-    end
+  hash = db["file:#{path}"]
+  if hash.nil?
+    $progress.log("Hashing local image file: #{path}")
+    hash = sha1_digest_of(File.read(path))
+    db["file:#{path}"] = hash
   end
+      
+  existing = db["photo:sha1:#{hash}"]
+
+  if existing.nil?
+    job = UploadJob.new(semaphore, queue, db, path, hash)
+    queue.enqueue_b { job.upload }
+    count = count + 1
+  else
+    #puts("Found exiting tag for #{hash} - not uploading")
+    $progress.increment
+  end
+  
 end
 
 queue.join
-
-
-
 
